2dd57e9a53c49df61589c860b77d1c2214144b1c
Sahar.sdr <sahar.sadrii.ss@gmail.com>
2025-03-03 21:43:36 +0000

    [Test] Add TaskList Test - remove test

--- src/test/java/TaskListTest.java
+++ src/test/java/TaskListTest.java
@@ -3,12 +3,14 @@ package test.java;
 import main.java.Task;
 import main.java.TaskList;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.After;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 
 public class TaskListTest {
@@ -21,6 +23,21 @@ public class TaskListTest {
         taskListTest.add("test", "test");
 
         assertEquals(initialSize+1, taskListTest.getSize());
+    }
+
+    @Test
+    public void testRemoveTask() {
+        List<Task> mockTasks = new ArrayList<>();
+        TaskList taskListTest = new TaskList(mockTasks);
+
+        int initialSize = taskListTest.getSize();
+        taskListTest.add("test", "test");
+        int id = taskListTest.getTasks().getFirst().getId();
+
+        assertEquals(initialSize+1, taskListTest.getSize());
+        assertTrue(taskListTest.remove(id));
+        assertEquals(initialSize, taskListTest.getSize());
+        assertFalse(taskListTest.remove(id));
     }
 
 }
